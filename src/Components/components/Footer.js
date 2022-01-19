aeed3b28049b057724790b04a819242aa8158dfa
Ben Gabriel <58117525+BenGabriel@users.noreply.github.com>
2022-01-18 22:16:17 -0800

    Update Footer.js

--- src/Components/components/Footer.js
+++ src/Components/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <p>&copy; Copyright Olloh Ukuku Ezekiel. All Rights reserved 2021</p>
+      <p>&copy; Copyright Odo John Chinonso 2017/251619. All Rights reserved 2022</p>
     </footer>
   );
 };
